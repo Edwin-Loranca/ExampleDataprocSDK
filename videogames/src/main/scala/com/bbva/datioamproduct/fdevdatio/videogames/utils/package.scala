e39d1aad8591645f3139fe3c3d6037df630a39dd
Edwin Loranca <eloranca@minsait.com>
2023-12-21 10:42:42 -0600

    commit2

---
+++ videogames/src/main/scala/com/bbva/datioamproduct/fdevdatio/videogames/utils/package.scala
@@ -0,0 +1,25 @@
+package com.bbva.datioamproduct.fdevdatio.videogames
+
+import com.bbva.datioamproduct.fdevdatio.videogames.common.ConfigConstants._
+import com.typesafe.config.Config
+import org.apache.spark.sql.{Dataset, Row}
+
+import scala.collection.convert.ImplicitConversions.`set asScala`
+
+package object utils {
+
+  implicit class SuperConfig(config: Config) extends IOUtils {
+
+    def readInputs: Map[String, Dataset[Row]] = {
+      config.getObject(InputTag).keySet()
+        .map(key => {
+
+          val inputConfig: Config = config.getConfig(s"$InputTag.$key")
+          val ds: Dataset[Row] = read(inputConfig)
+          key -> ds
+        }).toMap
+    }
+
+  }
+
+}
